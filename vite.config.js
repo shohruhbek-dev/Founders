3091d57e265c97c7958a3c71a568d0841aa26dc4
shohruhbek-dev <shohruhbek.abdusattarov.205@gmail.com>
2025-03-24 19:51:39 +0500

    change5

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,6 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
-import autoprefixer from 'autoprefixer'
-
-export default defineConfig({
-  plugins: [react(),
-  tailwindcss(),
-  autoprefixer()
-  ], 
-
-})
+export default {
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  },
+}
