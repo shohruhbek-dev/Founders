e9e9c37297eb2ce7b764752c18a0145331aed066
shohruhbek-dev <shohruhbek.abdusattarov.205@gmail.com>
2025-03-24 18:37:40 +0500

    change 2

--- src/root.jsx
+++ src/root.jsx
@@ -1,16 +1,9 @@
 import { Routes, Route } from "react-router-dom";
 import Layout from "./layout";
 import Home from "./pages/home";
-import { useEffect } from "react";
 
 function Root() {
-  useEffect(() => {
-    const images = document.querySelectorAll('img:not([loading])');
-    images.forEach(img => {
-      img.setAttribute('loading', 'lazy');
-    });
-  }, []);
-
+ 
   return (
     <Layout>
       <Routes>
